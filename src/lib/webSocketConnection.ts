635dbf24233cc4e3a6ebb72b6795718e7e762503
Sampie159 <38163547+Sampie159@users.noreply.github.com>
2023-05-21 23:01:10 -0300

    Can't be it can it?

--- src/lib/webSocketConnection.ts
+++ src/lib/webSocketConnection.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const endpoint = 'https://samschatroom.up.railway.app/' + process.env.PORT; // Railway url
+const endpoint = 'https://samschatroom.up.railway.app:' + process.env.PORT; // Railway url
 // const endpoint = 'http://localhost:3000'; // production url
 // const endpoint = 'http://localhost:5173'; // development url
 
