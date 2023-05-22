feecfba04b2caf6417a39926e4636e046feec387
Sampie159 <38163547+Sampie159@users.noreply.github.com>
2023-05-21 21:52:19 -0300

    prayge

--- src/lib/webSocketConnection.ts
+++ src/lib/webSocketConnection.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const endpoint = 'https://0.0.0.0:' + process.env.PORT; // Railway url
+const endpoint = 'http://0.0.0.0:' + process.env.PORT; // Railway url
 // const endpoint = 'http://localhost:3000'; // production url
 // const endpoint = 'http://localhost:5173'; // development url
 
