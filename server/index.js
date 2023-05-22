2b9029c47fe4c631f9d06c60da420eae093caf45
Sampie159 <38163547+Sampie159@users.noreply.github.com>
2023-05-21 21:10:24 -0300

    Fifth try

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createServer } from 'https';
+import { createServer } from 'http';
 import { Server } from 'socket.io';
 import { handler } from '../build/handler.js';
 
--- src/lib/webSocketConnection.ts
+++ src/lib/webSocketConnection.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const endpoint = 'https://localhost:' + process.env.PORT; // Railway url
+const endpoint = 'http://localhost:' + process.env.PORT; // Railway url
 // const endpoint = 'http://localhost:3000'; // production url
 // const endpoint = 'http://localhost:5173'; // development url
 
